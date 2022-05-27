a0e86bc3607366bb24081c1242cef9e85ce08a12
lee7198 <lee7198@naver.com>
2022-05-27 12:51:14 +0900

    init

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import "./App.css";
+import React, { useState } from "react";
+
+function App() {
+  let [num, setNum] = useState(0);
+
+  const setNumber = (e) => {
+    setNum(num + e);
+  };
+  return (
+    <div className="App">
+      <h1>WEEK 6 REACT COUNT NUMBER</h1>
+      <h3>{num}</h3>
+      <button
+        onClick={() => {
+          setNumber(1);
+        }}
+      >
+        +
+      </button>
+      <button
+        onClick={() => {
+          setNumber(-1);
+        }}
+      >
+        -
+      </button>
+    </div>
+  );
+}
+
+export default App;
